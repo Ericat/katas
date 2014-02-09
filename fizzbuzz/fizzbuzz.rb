7323baa4093c1fa61fb50703a5999b6fa2f1b79e
Ericat <erica.salvaneschi@hotmail.com>
2014-02-09 20:30:29 +0000

    Edited fizzbuzz

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,22 +1,22 @@
-def fizzbuzz(n)
-  class FizzBuzz
-    def self.fizzit!(n)
+class FizzBuzz
+
+    def fizzit!(n)
       return 'FizzBuzz' if is_divisible_by_both?(n)
       return 'Fizz' if is_divisible_by_three?(n)
       return 'Buzz' if is_divisible_by_five?(n)
       return n
     end
 
-    def self.is_divisible_by_both?(n) 
+    def is_divisible_by_both?(n) 
       n % 5 == 0 && n % 3 == 0
     end
 
-    def self.is_divisible_by_three?(n)
+    def is_divisible_by_three?(n)
       n % 3 == 0
     end
 
-    def self.is_divisible_by_five?(n)
+    def is_divisible_by_five?(n)
       n % 5 == 0
     end
-  end
+
 end
