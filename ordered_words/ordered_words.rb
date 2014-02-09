306d700e94af3d9ccedcb930ccf24e470b68695d
Erica Salvaneschi <erica.salvaneschi@hotmail.com>
2014-02-09 20:08:22 +0000

    Update ordered_words.rb

--- ordered_words/ordered_words.rb
+++ ordered_words/ordered_words.rb
@@ -1,2 +1,16 @@
 def ordered?(word)
+  alphabet = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+
+  alphabet_map = Hash.new{0}
+
+  alphabet.each_with_index do |value, index|
+    alphabet_map[value.downcase] = index
+  end
+
+  letters = word.downcase.chars
+
+  letter_values = letters.map{ |letter| alphabet_map[letter]}
+
+  letter_values == letter_values.sort ? true : false
+
 end
