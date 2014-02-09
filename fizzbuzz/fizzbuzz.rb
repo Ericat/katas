3f7a2e3a1306cfe0ab3d062c8c58783a9503da12
Erica Salvaneschi <erica.salvaneschi@hotmail.com>
2014-02-09 20:07:22 +0000

    Update fizzbuzz.rb

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,2 +1,22 @@
 def fizzbuzz(n)
+  class FizzBuzz
+  def self.fizzit!(n)
+    return 'FizzBuzz' if is_divisible_by_both?(n)
+    return 'Fizz' if is_divisible_by_three?(n)
+    return 'Buzz' if is_divisible_by_five?(n)
+    return n
+  end
+
+  def self.is_divisible_by_both?(n)   #undefined method `is_divisible_by_both' for FizzBuzz:Class if you dont put self
+    n % 5 == 0 && n % 3 == 0
+  end
+
+  def self.is_divisible_by_three?(n)
+    n % 3 == 0
+  end
+
+  def self.is_divisible_by_five?(n)
+    n % 5 == 0
+  end
+end
 end
